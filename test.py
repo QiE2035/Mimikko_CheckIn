5882e1c82f8c8976228aafe0b1ee22fddb59b646
企鹅2035 <qie2035@qq.com>
2020-11-15 18:31:21 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import os
 import urllib2
 
-url = "https://api1.mimikko.cn/client/love/ExchangeReward?code=momona&AppID=wjB7LOP2sYkaMGLC&Authorization="+os.environ["mimikko_data"]
+url1 = "https://api1.mimikko.cn/client/love/ExchangeReward?code="++os.environ["servant"]
+url2 = "&AppID=wjB7LOP2sYkaMGLC&Authorization="+os.environ["mimikko_data"]
 
-req = urllib2.Request(url)
+req = urllib2.Request(url1+url2)
 print req
 
 res_data = urllib2.urlopen(req)
